122b7dd61f96e5cecb44530b85f2faded8392116
max mcruvie <mcruvie@hotmail.co.uk>
2020-12-03 18:18:01 +0000

    Created "BaseViewModel"

---
+++ NBMSystem/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace NBMSystem.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChanged(string propertyChanged)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+    }
+}
