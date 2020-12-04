14eb9ce4cc26481d0a97264e961c61af80988e1a
max mcruvie <mcruvie@hotmail.co.uk>
2020-12-04 16:25:11 +0000

    Added TweetMessageType

--- NBMSystem/MessageTypes/TweetMessageType.cs
+++ NBMSystem/MessageTypes/TweetMessageType.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NBMSystem.Input;
 
 namespace NBMSystem.MessageTypes
 {
-    class TweetMessageType
+    public class TweetMessageType : MessageInput
     {
+        private string t_sender;
+        private string t_text;
+
+        public string TweetSender
+        {
+            get { return t_sender; }
+            set { if(!value.StartsWith("@") || value.Length > 15 || value.Length < 1)
+                {
+                    throw new ArgumentException("Error: The tweet sender must start with the @ symbol, and contain a maximum of 15 characters");
+                }
+            }
+        }
+
+        public string TweetText
+        {
+            get { return t_text; }
+            set { if(value.Length > 140 || value.Length < 1)
+                {
+                    throw new ArgumentException("Error: The tweet sender must start with the @ symbol, and be a length of less than 15");
+                } 
+
+            }
+        }
     }
 }
