f9082b84687bc6ecf9183816bdcde165c18b8b87
max mcruvie <mcruvie@hotmail.co.uk>
2020-12-04 15:16:45 +0000

    Created MessageInput class

---
+++ NBMSystem/Input/MessageInput.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBMSystem.Input
+{
+    public class MessageInput
+    {
+        private string m_header;
+        private string m_body;
+
+        public MessageInput()
+        {
+
+        }
+
+        public string header
+        {
+            get { return m_header; }
+            set
+            {
+                if (header.Length == 10)
+                {
+                    m_header = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Header must be length of 10");
+                }
+
+            }
+        }
+
+        public string body
+        {
+            get { return m_body; }
+            set { if (body.Length > 0)
+                {
+                    m_body = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Error: Body must not be empty");
+                }
+            }  
+        }
+    }
+}
