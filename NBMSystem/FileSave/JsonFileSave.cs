876f19dfb4758f58841666c3f1fbe3cf3a41dc21
max mcruvie <mcruvie@hotmail.co.uk>
2020-12-05 23:59:45 +0000

    Added JSON class with file
    Added Newtonshaft package to project

---
+++ NBMSystem/FileSave/JsonFileSave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NBMSystem.Input;
+using Newtonsoft.Json;
+
+namespace NBMSystem.FileSave
+{
+    public class JsonFileSave
+    {
+        public void saveToJson(List<MessageInput> message)
+        {
+            using (StreamWriter file = File.CreateText(@"../../../messages.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, message);
+            }
+        }
+    }
+}
